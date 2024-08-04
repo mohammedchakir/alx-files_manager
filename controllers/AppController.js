e1b752cfda2aafbef4551641231c0645cbd304fb
mohammedchakir <zerroukimedchakir@gmail.com>
2024-08-04 07:37:02 +0100

    add for check

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,13 +2,13 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  static getStatus(req, res) {
+  static getStatus (req, res) {
     const redisLive = redisClient.isAlive();
     const dbLive = dbClient.isAlive();
     res.status(200).json({ redis: redisLive, db: dbLive });
   }
 
-  static async getStats(req, res) {
+  static async getStats (req, res) {
     const usersTotal = await dbClient.nbUsers();
     const filesTotal = await dbClient.nbFiles();
     res.status(200).json({ users: usersTotal, files: filesTotal });
