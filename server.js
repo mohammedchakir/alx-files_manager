7695e24e263530557986d98b2b7b790264b3adb2
mohammedchakir <zerroukimedchakir@gmail.com>
2024-08-04 07:32:53 +0100

    add

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
-import { createClient } from 'redis';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
-
-const redisClient = createClient();
-const mongoUrl = 'mongodb://localhost:27017';
-const dbName = 'files_manager';
-let db;
-
-MongoClient.connect(mongoUrl, { useUnifiedTopology: true }, (err, client) => {
-  if (err) throw err;
-  db = client.db(dbName);
-});
-
-app.use((req, res, next) => {
-  req.db = db;
-  req.redisClient = redisClient;
-  next();
-});
 
 app.use('/', routes);
 
